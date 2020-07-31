40876e355efaffd8f9d77fd4f0c0a326cc718435
g.hatanaka <g.hatanaka@fbs.osaka-u.ac.jp>
2020-07-31 11:27:16 +0900

    Fix typo in Batch_MesCol_Measure.m

--- Batch_MesCol_Measure.m
+++ Batch_MesCol_Measure.m
@@ -9,7 +9,7 @@ mesStatus = mesCol.GetMesStatus();
 
 
 %% Set drawing parameters
-lins = (3:4:255)';
+lins = (17:17:255)';
 
 ColK = [000, 000, 000];
 KtoR = [lins, zero, zero];
@@ -43,39 +43,38 @@ propFile = fopen(propFileName, 'w');
 fclose(propFile);
 
 NofTrial = 1;
-wavelength=380:780;
 tic;
 cnt = 0;
 fprintf('# MesCol      : %d (%d x %d)\n', NofColor*NofTrial, NofColor, NofTrial);
 for ti = 1:NofTrial
-  len = randperm(NofColor);
-    for ci =  len
-      cnt = cnt + 1;
-        % Write draw script below
-        
-        
-        
-        
-        % Write draw script above
-        
-        mesCol.StartMes();
-        
-        colSpec = mesCol.GetColSpec(0);
-        specFile = fopen(specFileName, 'a');
-        fprintf(specFile, '%3d, %3d, %3d', ColorMap(ci, :));
-        fprintf(specFile, ', %E', ColSpec);
-        fprintf(specFile, newline);
-        fclose(specFile);
-        
-        colProp = mesCol.GetColProp(0);
-        propFile = fopen(propFileName, 'a');
-        fprintf(propFile, '%3d, %3d, %3d', ColorMap(ci, :));
-        fprintf(propFile, ', %f', [colProp{:}]);
-        fprintf(specFile, newline);        
-        fclose(propFile);
-        
-        fprintf('# M %8d  : %8.0f min, [%3d, %3d, %3d] -> Lv: %8.3f, x: %6.4f, y: %6.4f\n', cnt, toc/60, ColorMap(ci, :), colProp{2}, colProp{6}, colProp{7});
-    end
+  IdxColor = randperm(NofColor);
+  for ci =  IdxColor
+    cnt = cnt + 1;
+    % Write draw script below
+    
+    
+    
+    
+    % Write draw script above
+    
+    mesCol.StartMes();
+    
+    colSpec = mesCol.GetColSpec(0);
+    specFile = fopen(specFileName, 'a');
+    fprintf(specFile, '%3d, %3d, %3d', ColorMap(ci, :));
+    fprintf(specFile, ', %E', ColSpec);
+    fprintf(specFile, newline);
+    fclose(specFile);
+    
+    colProp = mesCol.GetColProp(0);
+    propFile = fopen(propFileName, 'a');
+    fprintf(propFile, '%3d, %3d, %3d', ColorMap(ci, :));
+    fprintf(propFile, ', %f', [colProp{:}]);
+    fprintf(specFile, newline);
+    fclose(propFile);
+    
+    fprintf('# M %8d  : %8.0f min, [%3d, %3d, %3d] -> Lv: %8.3f, x: %6.4f, y: %6.4f\n', cnt, toc/60, ColorMap(ci, :), colProp{2}, colProp{6}, colProp{7});
+  end
 end
 
 
@@ -88,4 +87,4 @@ end
 
 % Write finalize script above
 
-fprintf('# MesSpec     : %d (%d x %d), %8.0f min\n', NofColor*NofTrial, NofColor, NofTrial, toc/60);
+fprintf('# MesCol      : %d (%d x %d), %8.0f min\n', NofColor*NofTrial, NofColor, NofTrial, toc/60);
